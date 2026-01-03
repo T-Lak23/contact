d484879e804c5881a8875223fe10efb5c031c344
T-Lak23 <tilak230412@gmail.com>
2026-01-03 13:27:52 +0530

    adding catch all route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import ContactForm from "./pages/Contact";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<ContactForm />} />
+      <Route path="*" element={<Navigate to={"/"} replace />} />
     </Routes>
   );
 };
